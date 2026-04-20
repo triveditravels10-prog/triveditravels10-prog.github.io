21e1501b3c1ad7650cac065ee1eea82c3cd7826b
triveditravels10-prog <triveditravels10@gmail.com>
2026-04-20 16:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -126,8 +126,13 @@ window.onload = () => {
 
 
 
-
-
+//
+document.addEventListener("mousemove", e => {
+  const cursor = document.querySelector(".cursor");
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+//
 
 
 
