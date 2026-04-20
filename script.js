9475ac23120207e5e504f7e4e4e2933b0a6dc7e0
triveditravels10-prog <triveditravels10@gmail.com>
2026-04-20 16:35:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -116,23 +116,7 @@ window.onload = () => {
 
 
 
-//
-window.onload = () => {
-  document.body.classList.add("loaded");
-};
-//
 
-
-
-
-
-//
-document.addEventListener("mousemove", e => {
-  const cursor = document.querySelector(".cursor");
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});
-//
 
 
 
