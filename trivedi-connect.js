0a04bf4bdbdded2f2bc41d91ef55e237f30ccce7
triveditravels10-prog <triveditravels10@gmail.com>
2026-04-30 15:11:51 +0530

    Remove message sending and auto-reply logic
    Removed user message and auto-reply functionality from the chat.

--- trivedi-connect.js
+++ trivedi-connect.js
@@ -47,25 +47,7 @@ function sendMessage() {
 
   let box = document.getElementById("messages");
 
-  // user message
-  let messageDiv = document.createElement("div");
-  messageDiv.className = "message sent";
-  messageDiv.innerText = msg;
-  box.appendChild(messageDiv);
 
-  // auto reply
-  setTimeout(() => {
-    let reply = document.createElement("div");
-    reply.className = "message received";
-    reply.innerText = "Reply from Trivedi Connect";
-    box.appendChild(reply);
-
-    box.scrollTop = box.scrollHeight;
-  }, 1000);
-
-  box.scrollTop = box.scrollHeight;
-  document.getElementById("msg").value = "";
-}
 
 
 
